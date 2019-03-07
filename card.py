c55cafbbb8490b43d41d95136e9435c77811e5d4
Lawrence Jones <git@lwjon.es>
2019-03-06 22:27:10 -0800

    Created the basic layer of a card; ensure it works too

---
+++ card.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+VALUES = "A23456789TJQK"
+DEFAULT_VALUE = "A"
+Suit = Enum("Suit", "clubs diamonds hearts spades")
+DEFAULT_SUIT = Suit.spades
+
+
+class Card:
+    def __init__(self, suit=DEFAULT_SUIT, value=DEFAULT_VALUE):
+        self.suit = suit.name
+        self.value = value
+
+    def __repr__(self):
+        return "{value} of {suit}".format(value=str(self.value), suit=self.suit.title())
---
+++ main.py
@@ -0,0 +1,9 @@
+from card import Card, Suit
+
+
+
+
+newCard = Card(Suit.clubs, "3")
+print(newCard)
+defaultCard = Card()
+print(defaultCard)
